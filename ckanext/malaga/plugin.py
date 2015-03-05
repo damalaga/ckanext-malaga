1a5157e154366d7f8adaeb65d6ab24b9d1012e7b
CKAN user <ckan@mv0115.aytomalaga.intranet>
2015-03-05 10:21:02 +0100

    Modificaciones varias y uso de ckanext-contact

--- ckanext/malaga/plugin.py
+++ ckanext/malaga/plugin.py
@@ -8,7 +8,6 @@
 
 import ckan.plugins as p
 
-import convertfile as conv
 import federador as fed
 
 import os
@@ -17,16 +16,9 @@ import ckan.plugins.toolkit as toolkit
 import ckan.lib.base as base
 
 import urllib
+import ckan.lib.helpers as ckanhelper
 
-
-# mlg_count_dataset: Devuelve el numero de datasets.
-# mlg_count_dataset: Returns numbers of datasets. 
-def mlg_count_datasets():
-
-	data_dict = {}
-
-	response = toolkit.get_action('package_list')(data_dict=data_dict)
-	return len(response)
+import operator #usado para ordenar los tags en orden ascendente // used asc tags order
 
 # mlg_count_resources: Devuelve el numero de recursos.
 # mlg_count_resources: Returns numbers of resources
@@ -78,21 +70,33 @@ def mlg_cur_datetime (self):
 def mlg_datasets_resources_list():
 	return toolkit.get_action('current_package_list_with_resources')(data_dict={})
 
-#################################
-def mlgconv_csv_to_xml(resourceid):
-	return conv.csv_to_xml(resourceid)
-
 # mlg_group_list: Devuelve una lista con los grupos
 # mlg_group_list: Returns list of groups 
 def mlg_group_list():
 	data_dict = {'all_fields': True} #all_fields returns each group field (name, url, image,...).
 	return toolkit.get_action('group_list')(data_dict=data_dict)
 
-# mlg_group_list: Devuelve una lista con las organizaciones
-# mlg_group_list: Returns list of organization
+# mlg_organization_list Devuelve una lista con las organizaciones
+# mlg_organization_list: Returns list of organization
 def mlg_organization_list():
 	data_dict = {'all_fields': True} #all_fields returns each group field (name, url, image,...).
 	return toolkit.get_action('organization_list')(data_dict=data_dict)
+
+# mlg_get_most_used_tags: Devuelve una lista de las etiquetas mas usadas y el numero de items que la usan
+# mlg_get_most_used_tags: Returns list of the most used tags, and numbers of items
+def mlg_get_most_used_tags(limit):
+	lists = mlg_datasets_resources_list()
+	hashtags = {}
+	for ds in lists:
+		if ds['tags']:
+			for item in ds['tags']:
+				if item['name']:
+					if (item['name'] in hashtags):
+						hashtags[item['name']] = hashtags[item['name']] + 1
+					else:
+						hashtags[item['name']] = 1
+
+	return sorted(hashtags.items(), key=operator.itemgetter(1), reverse=True)[:limit]
 
 
 class malagae(p.SingletonPlugin):
@@ -103,6 +107,7 @@ class malagae(p.SingletonPlugin):
 
     p.implements(p.IRoutes, inherit=True)
 
+
 #iruiz: Redefinir before_map para
 # apl = nueva entrada en el menu, que se llama aplicaciones.
 # federador = los ficheros que se usan en la federacion
@@ -112,30 +117,25 @@ class malagae(p.SingletonPlugin):
 
 	import pylons.config as config 
 
-#loading configure parameters.
-	apl_url_config = config['ckan_mlg.apl_url']                            #loading url ckan must open
-	federador_fname_config = config['ckan_mlg.federador_rdf_write']        #loading rdf filename (include path)
-	federador_fname_url = config['ckan_mlg.federador_rdf_url']             #loading URL rdf
-	federador_template_config = config['ckan_mlg.federador_template']      #loading rdf template filename (include path)
-	federador_response_config = config['ckan_mlg.federador_htmlresponse']  #loading htlm file response (include path)
-
-
 # Redefine before_map: include aplicaciones on main menu
+	apl_url_config = config['ckan_mlg.apl_url']                     #loading url ckan must open
+
 	m.connect('aplicaciones', #name of path route
 	'/aplicaciones', #url to map path to
 	controller='ckanext.malaga.controller:AplicacionesController', #controller
 	action='aplicaciones', apl_url=apl_url_config) #controller action (method)
 
 # Redefine before_map: URL /local/generador execute GenerarRDF and generate rdf file used on datos.gob.es federacion
-	m.connect('generador', #name of path route
-	'/local/generador', #url to map path to
+	federador_fname = config['ckan_mlg.federador_file']             #rdf filename (include path)
+	federador_template = config['ckan_mlg.federador_template']      #loading rdf template filename (include path)
+	federador_process = config['ckan_mlg.federador_process']        #federador process
+
+	m.connect(federador_process, #name of path route
+	'/'+federador_process, #url to map path to
 	controller='ckanext.malaga.generadorrdf:GenerarRDF', #controller
-	action='generar',fname=federador_fname_config, template=federador_template_config) #controller
+	action='generar',fname=federador_fname, template=federador_template) #controller
 
 	return m
-
-    def configure(self, config):
-	self.number_max = config.get('ckan.num_max_datasets')
 
     def update_config(self, config):
         # add template directory that contains our snippet
@@ -144,8 +144,7 @@ class malagae(p.SingletonPlugin):
 
     #iruiz: register helper function
     def get_helpers(self):
-        return {'mlg_count_datasets':mlg_count_datasets,
-		'mlg_count_resources':mlg_count_resources,
+        return {'mlg_count_resources':mlg_count_resources,
 		'mlg_tracking_summary': mlg_tracking_summary,
 		'mlg_most_downloaded': mlg_most_downloaded,
 		'mlg_cur_datetime': mlg_cur_datetime,
@@ -154,4 +153,6 @@ class malagae(p.SingletonPlugin):
 		'mlg_group_list': mlg_group_list,
 		'mlg_organization_list': mlg_organization_list,
 		'mlg_datasets_resources_list': mlg_datasets_resources_list,
-		'mlgconv_csv_to_xml': mlgconv_csv_to_xml}
+		'mlg_get_most_used_tags':mlg_get_most_used_tags,
+		'mlg_federador_value':fed.mlg_federador_value
+}
